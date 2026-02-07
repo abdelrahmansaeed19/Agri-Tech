2676cb7b8f78b4be57b9a3b7272e8bf6b9633991
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2026-02-07 19:20:12 +0200

    Deploy To Azure Cloud

---
+++ Data/Migrations/20260207171428_CreateDB_in_Azure.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriculturalTech.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDB_in_Azure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Humidity",
+                table: "WeatherForecasts");
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 7, 17, 14, 27, 669, DateTimeKind.Utc).AddTicks(1537));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 7, 17, 14, 27, 669, DateTimeKind.Utc).AddTicks(1554));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 7, 17, 14, 27, 669, DateTimeKind.Utc).AddTicks(1561));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 7, 17, 14, 27, 669, DateTimeKind.Utc).AddTicks(1565));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 2, 7, 17, 14, 27, 669, DateTimeKind.Utc).AddTicks(1588));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Humidity",
+                table: "WeatherForecasts",
+                type: "decimal(5,2)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 29, 19, 0, 38, 413, DateTimeKind.Utc).AddTicks(6221));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 29, 19, 0, 38, 413, DateTimeKind.Utc).AddTicks(6239));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 29, 19, 0, 38, 413, DateTimeKind.Utc).AddTicks(6244));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 29, 19, 0, 38, 413, DateTimeKind.Utc).AddTicks(6249));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2025, 10, 29, 19, 0, 38, 413, DateTimeKind.Utc).AddTicks(6253));
+        }
+    }
+}
