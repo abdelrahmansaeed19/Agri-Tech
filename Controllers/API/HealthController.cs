e23c5c51ff3fff7e7f0103b4f99c8faa96f79954
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2026-02-21 10:55:13 +0200

    add continuous ping con

---
+++ Controllers/API/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AgriculturalTech.API.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // cron-job.org will send a GET request here every 14 minutes
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            // Returns a simple 200 OK with no database calls and almost zero CPU usage
+            return Ok(new { status = "awake", time = DateTime.UtcNow });
+        }
+    }
+}
