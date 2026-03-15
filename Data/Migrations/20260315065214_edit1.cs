8111ada06ed3ee01eb1f32a8e91ab695942a4373
abdelrahmansaeed19 <abdelrahman.saeed.khater@gmail.com>
2026-03-15 08:52:58 +0200

    edit

---
+++ Data/Migrations/20260315065214_edit1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AgriculturalTech.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SubscriptionStatus",
+                table: "UserSubscription",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 15, 6, 52, 13, 800, DateTimeKind.Utc).AddTicks(1843));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 15, 6, 52, 13, 800, DateTimeKind.Utc).AddTicks(1858));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 15, 6, 52, 13, 800, DateTimeKind.Utc).AddTicks(1863));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 15, 6, 52, 13, 800, DateTimeKind.Utc).AddTicks(1867));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 15, 6, 52, 13, 800, DateTimeKind.Utc).AddTicks(1870));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "SubscriptionStatus",
+                table: "UserSubscription",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 8, 57, 56, 308, DateTimeKind.Utc).AddTicks(113));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 8, 57, 56, 308, DateTimeKind.Utc).AddTicks(128));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 8, 57, 56, 308, DateTimeKind.Utc).AddTicks(132));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 8, 57, 56, 308, DateTimeKind.Utc).AddTicks(135));
+
+            migrationBuilder.UpdateData(
+                table: "CropTypes",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedAt",
+                value: new DateTime(2026, 3, 12, 8, 57, 56, 308, DateTimeKind.Utc).AddTicks(138));
+        }
+    }
+}
